501ff485ad5bb2386c19894694af872eae82a703
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-08-13 01:48:40 +0600

    firebase setup completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  	return (
+	  	<>
+		  	<Head> 
+				<link rel="icon" type="image/png" href="/fclogo.png"/>
+			</Head>
+  			<Component {...pageProps} />
+  		</>
+	)
 }
 
 export default MyApp;
