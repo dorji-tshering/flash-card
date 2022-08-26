2fe426ef728b31ba256bc86b6d176c725c3cf827
Dorji Tshering <dorjitshering370@gmail.com>
2022-08-27 02:33:16 +0600

    Update firebaseAdmin.ts

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -9,7 +9,7 @@ if(!admin.apps.length) {
         credential : admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY : undefined,
         }),
         databaseURL: process.env.DATABASE_URL,
     });
