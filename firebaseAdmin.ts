3e3b4a7700787ba3f482fb8a7d97eca51f279fb2
Dorji Tshering <dorjitshering370@gmail.com>
2022-08-27 02:27:08 +0600

    parsed private key for production only

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -9,8 +9,7 @@ if(!admin.apps.length) {
         credential : admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            // replace `\` and `n` character pairs w/ single `\n` character
-            privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
         }),
         databaseURL: process.env.DATABASE_URL,
     });
